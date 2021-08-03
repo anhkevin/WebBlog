446407a139b806e3f9ce960aa95f283f5fdee586
anhkevin <anhkevin@users.noreply.github.com>
2021-08-03 14:39:32 +0000

    deploy: 004157e80f51b3ec8b5d8da869a3ad7da0c94b7a

---
+++ _nuxt/static/1628001563/defer-parsing-of-javascript-trong-wordpress/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1628001563",layout:"default",error:a,state:{search:""},serverRendered:true,routePath:"\u002Fdefer-parsing-of-javascript-trong-wordpress\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b2178281"}}}}(null));
