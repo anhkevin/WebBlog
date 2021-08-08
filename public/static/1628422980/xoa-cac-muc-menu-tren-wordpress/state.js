de81b18bf2ff022a22503c59bae6e9ab70134dc9
anhkevin <anhkevin@users.noreply.github.com>
2021-08-08 11:43:05 +0000

    deploy: aea0460cdb6458a65bf45854ddd53d3824c66788

---
+++ public/static/1628422980/xoa-cac-muc-menu-tren-wordpress/state.js
@@ -0,0 +1,1 @@
+window.__TIANDEV__=(function(a){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1628422980",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:"\u002Fxoa-cac-muc-menu-tren-wordpress\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"8f6c8bc2"}}}}(null));
