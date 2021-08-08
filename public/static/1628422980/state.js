c31f048474a2bf0c4443bee7d7f7b0552a7eb8e3
anhkevin <anhkevin@users.noreply.github.com>
2021-08-08 15:57:48 +0000

    deploy: 5724a3039de3bb65e9dfa49d35b18ccdc0c99a0d

--- public/static/1628422980/state.js
+++
@@ -1,1 +0,0 @@
-window.__TIANDEV__=(function(a,b){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1628422980",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"8f6c8bc2"}}}}(null,"\u002F"));
--- public/static/1628422980/toi-uu-va-tang-toc-wordpress-bang-plugin-autoptimize/state.js
+++
@@ -1,1 +0,0 @@
-window.__TIANDEV__=(function(a){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1628422980",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:"\u002Ftoi-uu-va-tang-toc-wordpress-bang-plugin-autoptimize\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"8f6c8bc2"}}}}(null));
