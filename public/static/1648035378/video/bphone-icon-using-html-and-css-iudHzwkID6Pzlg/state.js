2a9053f76c54558abdb511f75c74c8f0c9e76491
anhkevin <anhkevin@users.noreply.github.com>
2022-03-23 11:36:24 +0000

    deploy: a3c27ef5dbb141d033bad939fa89a3f2cde47ced

---
+++ public/static/1648035378/video/bphone-icon-using-html-and-css-iudHzwkID6Pzlg/state.js
@@ -0,0 +1,1 @@
+window.__TIANDEV__=(function(a){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1648035378",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:"\u002Fvideo\u002Fbphone-icon-using-html-and-css-iudHzwkID6Pzlg\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"3ba936fe"}}}}(null));
