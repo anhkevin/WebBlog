6594fea1017f182ec0aa9ade40dff65e107de3f1
anhkevin <anhkevin@users.noreply.github.com>
2022-03-25 02:10:46 +0000

    deploy: d7a6f4f634155ed81c373e6c0151227ff5e2afbf

--- public/static/1648038846/state.js
+++
@@ -1,1 +0,0 @@
-window.__TIANDEV__=(function(a,b){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1648038846",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"e067492f"}}}}(null,"\u002F"));
---
+++ public/static/1648174242/php-8-co-gi-moi/state.js
@@ -0,0 +1,1 @@
+window.__TIANDEV__=(function(a){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1648174242",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:"\u002Fphp-8-co-gi-moi\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"9cee1292"}}}}(null));
