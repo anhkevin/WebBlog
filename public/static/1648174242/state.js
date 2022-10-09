905204afe0c01b64ad7e80cb99c0608958250e0f
anhkevin <anhkevin@users.noreply.github.com>
2022-10-09 13:48:49 +0000

    deploy: 8c7998bfa542b5f1c40c0c0984d23ca8308eadbb

--- public/static/1648174242/state.js
+++
@@ -1,1 +0,0 @@
-window.__TIANDEV__=(function(a,b){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1648174242",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"9cee1292"}}}}(null,"\u002F"));
