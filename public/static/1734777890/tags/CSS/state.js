a66e3ab52a71e413c52a4a53007b653f25828a66
anhkevin <anhkevin@users.noreply.github.com>
2024-12-21 10:44:54 +0000

    deploy: c64d2a0b738576d1e7865e318b46e75dd0153a46

---
+++ public/static/1734777890/tags/CSS/state.js
@@ -0,0 +1,1 @@
+window.__TIANDEV__=(function(a){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1734777890",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:"\u002Ftags\u002FCSS\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"2f215765"}}}}(null));
