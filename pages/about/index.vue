d7f00cb49816d18ff2e940a156988b645ad0a81d
anhkevin.ht@gmail.com <anhkevin.ht@gmail.com>
2024-12-21 22:03:54 +0700

    Update: About Me

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -6,44 +6,46 @@
             <div class="container">
                <div class="profile-teaser row">
                   <div class="col-md-6 col-lg-6">
-                     <h2 class="name font-weight-bold mb-1">TianDev</h2>
-                     <div class="tagline mb-3">Web Developer</div>
+                     <h2 class="name font-weight-bold mb-1">About Me</h2>
                      <div class="bio mb-4">
-                        Xin chào!<br>
-                        Tôi là TianDev và là một Web Developer.<br>
-                        Vào thời gian rãnh rỗi tôi thích viết lại kinh nghiệm hay vấn đề đã từng vấp phải và blog này chính là một "developer notes" của tôi
+                        I’m TianDev, a developer with 9 years of experience. I specialize in creating bespoke web solutions that not only meet client expectations but also set new standards for innovation and excellence.
                      </div>
-                     <!--//bio-->
-                     <!-- <div class="mb-4">
-                        <a class="btn btn-primary me-2 mb-3" href="portfolio.html">
-                           <svg class="svg-inline--fa fa-arrow-alt-circle-right fa-w-16 me-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="arrow-alt-circle-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
-                              <path fill="currentColor" d="M256 8c137 0 248 111 248 248S393 504 256 504 8 393 8 256 119 8 256 8zM140 300h116v70.9c0 10.7 13 16.1 20.5 8.5l114.3-114.9c4.7-4.7 4.7-12.2 0-16.9l-114.3-115c-7.6-7.6-20.5-2.2-20.5 8.5V212H140c-6.6 0-12 5.4-12 12v64c0 6.6 5.4 12 12 12z"></path>
-                           </svg>
-                           <span class="d-none d-md-inline">View</span> Portfolio
-                        </a>
-                        <a class="btn btn-secondary mb-3" href="resume.html">
-                           <svg class="svg-inline--fa fa-file-alt fa-w-12 me-2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="file-alt" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" data-fa-i2svg="">
-                              <path fill="currentColor" d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm64 236c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12v8zm0-64c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12v8zm0-72v8c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-8c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12zm96-114.1v6.1H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z"></path>
-                           </svg>
-                           <span class="d-none d-md-inline">View</span> Resume
-                        </a>
-                     </div> -->
+                     <div class="tagline mb-3">Web Development</div>
+                     <p>Proficient in crafting responsive and interactive websites that deliver seamless user experiences across all devices.</p>
                   </div>
                   <!--//col-->
                   <div class="col-md-6 col-lg-6 mb-3">
                      <img src="/img/tiandev_card.png" alt="avatar tiandev">
                   </div>
                </div>
+
+               <div class="profile-teaser row">
+                  <h2 class="section-title font-weight-bold mb-3">Expertise</h2>
+                  <div class="col-md-6 col-lg-6">
+                     <div class="tagline mb-3">Web Development</div>
+                     <p>Proficient in crafting responsive and interactive websites that deliver seamless user experiences across all devices.</p>
+                  </div>
+                  <div class="col-md-6 col-lg-6">
+                     <div class="tagline mb-3">PHP Programming</div>
+                     <p>Extensive experience in PHP to build robust, server-side applications that are both high-performing and scalable.</p>
+                  </div>
+                  <div class="col-md-6 col-lg-6">
+                     <div class="tagline mb-3">WordPress Solutions</div>
+                     <p>Skilled in developing custom WordPress themes and plugins to enhance functionality and cater to specific business needs.</p>
+                  </div>
+                  <div class="col-md-6 col-lg-6">
+                     <div class="tagline mb-3">Laravel Framework</div>
+                     <p>Expertise in utilizing Laravel to create elegant and efficient web applications, ensuring code quality and application reliability.</p>
+                  </div>
+               </div>
+
                <div class="profile-teaser row">
                 <div class="col">
-                   <h2 class="section-title font-weight-bold mb-3">Về blog TianDev</h2>
+                   <h2 class="section-title font-weight-bold mb-3">Contact</h2>
                    <div class="section-intro mb-5">
-                      Với nhiều năm trong lĩnh vực lập trình cá nhân tôi nhận thấy kiến thức lập trình là vô hạn. Một số kiến thức và kinh nghiệm tích lũy được nếu không được ứng dụng thường xuyên sẽ dần bị phai mờ đi sau một thời gian.<br>
-                      Cụ thể tôi đã từng mất rất nhiều thời gian để research lại một vấn đề gì đó đã từng gặp phải hoặc đã từng research rất lâu trước đó<br>
-                      <br>
-                      Đó là lý do blog này của tôi ra đời mục đích lưu trữ lại một số kiến thức research được hoặc những kinh nghiệm trải qua vào blog này, không chỉ lưu trữ lại mà còn giúp tôi một lần nữa khắc sâu hơn.
-                      <br><br>
-                      Tôi mong rằng đây không chỉ là "Developer notes" cho cá nhân tôi mà có thể là nguồn tham khảo cho ai đó cần.
+                     Feel free to reach out to discuss projects, collaborations, or just to connect!<br>
+                     <b>Email:</b> anhkevin.ht@gmail.com <br>
+                     Let's innovate together and turn your ideas into reality!
                    </div>
                 </div>
              </div>
