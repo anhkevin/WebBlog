12dc8fe8ebb42af7dca9d72aa37ba9a85ccb6156
anhkevin <anhkevin@users.noreply.github.com>
2024-12-29 05:41:00 +0000

    deploy: e169bde510370b906ecd02ccebcf55206c65b3f0

---
+++ public/static/1735450856/thiet-lap-basic-authentication-bang-php/state.js
@@ -0,0 +1,1 @@
+window.__TIANDEV__=(function(a){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1735450856",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:"\u002Fthiet-lap-basic-authentication-bang-php\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"6a2f89d6"}}}}(null));
