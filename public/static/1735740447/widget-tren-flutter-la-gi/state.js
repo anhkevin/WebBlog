ebde495422e7c2ad08bbeaeb0f543d6e6c7d3535
anhkevin <anhkevin@users.noreply.github.com>
2025-01-01 14:07:30 +0000

    deploy: c23b7dd2258b1c9799886b30bbe66de54d934837

---
+++ public/static/1735740447/widget-tren-flutter-la-gi/state.js
@@ -0,0 +1,1 @@
+window.__TIANDEV__=(function(a){return {staticAssetsBase:"\u002Fpublic\u002Fstatic\u002F1735740447",layout:"default",error:a,state:{search:"",menu:false},serverRendered:true,routePath:"\u002Fwidget-tren-flutter-la-gi\u002F",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fpublic\u002F",cdnURL:a},content:{dbHash:"012f952b"}}}}(null));
